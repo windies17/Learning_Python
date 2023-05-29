e7d173bcb4c717665fecf1193d1c335792b8dc31
windies17 <craigwilson1980@gmail.com>
2023-05-29 10:23:26 +0100

    The new list has been updated

---
+++ Lists.py
@@ -0,0 +1,10 @@
+#In python you can lists
+#lists can be iterated like strings like so
+
+the_new_list = ["John","James","Jack"]
+
+# the_new_list.append["Jim"] # I put square brackets when it should have been parentheses ()
+
+the_new_list.append("Jim")
+
+print(the_new_list)
